4dd5a70ebbaff780b81a88d2392cbeee14965bc4
Ricardo Simões Costa <ricardo@ricardos-mbp.lan>
2023-09-28 23:21:43 +0100

    module federation with react

---
+++ remote/src/Item.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const Item = () => (
+	<div className="mt-10 text-3xl mx-auto max-w-6xl bg-red-400">
+		<div>Itemx</div>
+	</div>
+);
+export default Item;
